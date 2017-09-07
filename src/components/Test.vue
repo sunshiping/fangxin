e73a2d76c16f9037f9424a80a667ffedfee2a4a9
xuefenhong <979671647@qq.com>
2017-09-07 17:55:49 +0800

    列表添加滚动条

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,18 +1,62 @@
 <template>
     <div>
-        <fx-panel :houseList="houseList"></fx-panel>
+        <scroller lock-x :scrollbar-y=true>
+            <fx-panel :houseList="houseList"></fx-panel>
+        </scroller>
     </div>
 </template>
 
 <script>
+    import { Scroller } from 'vux'
     import FxPanel from './common/fx-panel.vue'
     export default {
       components: {
-        FxPanel
+        FxPanel,
+        Scroller
       },
       data () {
         return {
           houseList: [
+            {
+              name: '龙腾西城  3室2厅 89.61㎡ 龙腾西城',
+              price: '240',
+              unitPrice: '15000',
+              desc: ['南北通透', '高层(共30层)', '2014'],
+              tags: ['学区房', '地铁房'],
+              seeCount: 20,
+              stars: 3.5,
+              score: 4.8
+            },
+            {
+              name: '龙腾西城  3室2厅 89.61㎡ 龙腾西城',
+              price: '240',
+              unitPrice: '15000',
+              desc: ['南北通透', '高层(共20层)', '2015'],
+              tags: ['学区房1', '地铁房1'],
+              seeCount: 23,
+              stars: 4.5,
+              score: 4.9
+            },
+            {
+              name: '龙腾西城  3室2厅 89.61㎡ 龙腾西城',
+              price: '240',
+              unitPrice: '15000',
+              desc: ['南北通透', '高层(共30层)', '2014'],
+              tags: ['学区房', '地铁房'],
+              seeCount: 20,
+              stars: 3.5,
+              score: 4.8
+            },
+            {
+              name: '龙腾西城  3室2厅 89.61㎡ 龙腾西城',
+              price: '240',
+              unitPrice: '15000',
+              desc: ['南北通透', '高层(共20层)', '2015'],
+              tags: ['学区房1', '地铁房1'],
+              seeCount: 23,
+              stars: 4.5,
+              score: 4.9
+            },
             {
               name: '龙腾西城  3室2厅 89.61㎡ 龙腾西城',
               price: '240',
