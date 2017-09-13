92dc53e60e10af7360fabc78fce5a07b52b6eb4e
xuefenhong <979671647@qq.com>
2017-09-13 17:14:38 +0800

    test

--- src/router/Source/RouterSource.js
+++ src/router/Source/RouterSource.js
@@ -1,6 +1,6 @@
 import Source from './../../components/Source/Source.vue'
 import SourceList from './../../components/SourceList.vue'
-import Test from './../../components/Test.vue'
+import Test from '../../components/Test.vue'
 import List from '../../components/common/ListHeader.vue'
 import SourceInfo from '../../components/SourceInfo.vue'
 import PersonalInfo from '../../components/PersonalInfo.vue'
