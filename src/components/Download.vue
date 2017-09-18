f5ec3fb759c3ec2cd57078d23d64ddb1551420fa
duwanwan <duwanwan>
2017-09-18 17:24:53 +0800

    评论页面和下载页面

---
+++ src/components/Download.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="down-content">
+        <div class="header-img">
+            <img src="../../static/login/logo.png" />
+        </div>
+        <div class="text-cont">
+            <p class="title">房信网-专业的房产交易平台</p>
+            <p class="desc">下载APP 享受更优质的贴心服务</p>
+        </div>
+
+        <div class="down">
+            <span class="down-android">立即下载</span>
+            <span class="down-ios">立即下载</span>
+        </div>
+    </div>
+</template>
+<script></script>
+<style>
+    .down-content{
+         background-color: #ffffff;
+         height: 100%;
+         text-align: center;
+     }
+    .header-img{
+        padding: 50px 0 30px 0;
+        text-align: center;
+    }
+    .header-img img{
+        width: 20%;
+    }
+    h1{text-align: center}
+</style>
---
+++ src/router/Down/RouterDown.js
@@ -0,0 +1,7 @@
+import Download from '../../components/Download.vue'
+
+export default [{
+  path: '/down',
+  name: '下载',
+  component: Download
+}]
